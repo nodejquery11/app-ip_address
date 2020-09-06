e8bd9ada7442b351adbd2d96862e08f9a4f5d1a4
nodejquery11 <nodejquery11@gmail.com>
2020-09-06 15:21:10 +0000

    add main.js

--- main.js
+++ main.js
@@ -62,11 +62,16 @@ const path = require('path');
  */
 const { getIpv4MappedIpv6Address } = require(path.join(__dirname, 'ipv6.js'));
 
-/*
-  This section is used to test function and log any errors.
-  We will make several positive and negative tests.
-*/
-function main() {
+class IpAddress {
+  constructor() {
+    // IAP's global log object is used to output errors, warnings, and other
+    // information to the console, IAP's log files, or a Syslog server.
+    // For more information, consult the Log Class guide on the Itential
+    // Developer Hub https://developer.itential.io/ located
+    // under Documentation -> Developer Guides -> Log Class Guide
+    log.info('Starting the IpAddress product.');
+  }
+  main() {
   // Create some test data for getFirstIpAddress(), both valid and invalid.
   let sampleCidrs = ['172.16.10.0/24', '172.16.10.0 255.255.255.0', '172.16.10.128/25', '192.168.1.216/30'];
   let sampleCidrsLen = sampleCidrs.length;
@@ -100,7 +105,6 @@ function main() {
     }
   }
 }
-/*
-  Call main to run it.
-*/
-main();
+}
+
+module.exports = new IpAddress;
